db60f630522a84fec1bb0e95718eba9aadbef90c
nina <nina.pauly48@gmail.com>
2023-04-30 11:36:11 +0200

    Revert "Delete FrmGestionActivite.cs"
    This reverts commit d59a36fb240d745723389492e5beebb3163b6dfc.

---
+++ GestionOrganisationCongres/FrmGestionActivite.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionOrganisationCongres
+{
+    
+    public partial class FrmGestionActivite : Form
+    {
+        gestionCongresEntities context;
+        public FrmGestionActivite()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGestionActivite_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                context = new gestionCongresEntities();
+
+                context.Activites.Load();
+
+                bindSrcActivites.DataSource = context.Activites.Local.ToBindingList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void bindSrcActivites_CurrentChanged(object sender, EventArgs e)
+        {
+            bindSrcInscriptions.DataSource = ((Activite)bindSrcActivites.Current).Congressistes.ToList();
+        }
+    }
+}
